476109ee984a284f5e835a1d0e79dba92629ecdb
TaylorYates <taylor.yates@live.com>
2018-08-05 16:12:03 +1000

    Created header component, added styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Quotes from './components/quotes'
 import PillButtons from './components/pillButtons'
-
+import Header from './components/header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="App-title">Shakespeare project</h1>
+        <div className="topContainer">
+          <Header/>
+          <PillButtons/>
+        </div>
         <Quotes />
-        <PillButtons/>
       </div>
     );
   }
---
+++ src/components/header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './header.css';
+
+class Header extends React.Component {
+
+  render() {
+    return (
+      <div className="header">
+        <h1>Henry IV, Part 1</h1>
+        <p>Emotional analysis</p>
+      </div>
+    )
+  }
+}
+
+export default Header;
